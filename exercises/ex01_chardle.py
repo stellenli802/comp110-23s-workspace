b69496aa25be8015cd9d33d4f2961b8b542d4c62
Stellen Li <your@primary.email>
2023-01-19 13:31:39 -0500

    ex01 chardle draft

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,6 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = "730476572"
+
+five_character: str = input("Enter a five character word: ")
+single_character: str = input("Enter a single character: ")
