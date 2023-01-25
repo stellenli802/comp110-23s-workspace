1d2e24d49e97ab5ebf880f2234998f99b545ef3d
Stellen Li <your@primary.email>
2023-01-25 02:22:06 -0500

    Final ex01_chardle

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,6 +1,37 @@
 """EX01 - Chardle - A cute step toward Wordle."""
 
 __author__ = "730476572"
-
+#Sets up variables
 five_character: str = input("Enter a five character word: ")
 single_character: str = input("Enter a single character: ")
+
+if(len(five_character) != 5):
+    print("Error: Word must contain 5 characters.")
+elif(len(single_character) != 1):
+    print("Error: Input must be a single character.")
+    print("Searching for " + single_character + " in " + five_character)
+
+index: int = 0
+count: int = 0
+result: str = str(index)
+if(single_character == five_character[0]):
+    print(single_character + " found at index " + str(index))
+    count = count + 1
+index = index + 1
+if(single_character == five_character[1]):
+    print(single_character + " found at index " + str(index))
+    count = count + 1
+index = index + 1
+if(single_character == five_character[2]):
+    print(single_character + " found at index " + str(index))
+    count = count + 1
+index = index + 1
+if(single_character == five_character[3]):
+    print(single_character + " found at index " + str(index))
+    count = count + 1
+index = index + 1
+if(single_character == five_character[4]):
+    print(single_character + " found at index " + str(index))
+    count = count + 1
+index = index + 1
+print(str(count) + " instances of " + single_character + " found in " + five_character)
