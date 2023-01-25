598b9538bf2fa39f026f729700aeea2595b7705b
Stellen Li <your@primary.email>
2023-01-25 02:43:37 -0500

    draft_ex01_chardle

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,37 +1,45 @@
 """EX01 - Chardle - A cute step toward Wordle."""
 
 __author__ = "730476572"
-#Sets up variables
+import sys
+# Sets up variables
 five_character: str = input("Enter a 5-character word: ")
 single_character: str = input("Enter a single character: ")
 
-if(len(five_character) != 5):
+if (len(five_character) != 5):
+    sys.exit("Error: Word must contain 5 characters.")
     print("Error: Word must contain 5 characters.")
-elif(len(single_character) != 1):
-    print("Error: Input must be a single character.")
-    print("Searching for " + single_character + " in " + five_character)
+elif (len(single_character) != 1):
+    print(exit)
+    raise SystemExit
+print("Searching for " + single_character + " in " + five_character)
 
 index: int = 0
 count: int = 0
 result: str = str(index)
-if(single_character == five_character[0]):
+if (single_character == five_character[0]):
     print(single_character + " found at index " + str(index))
     count = count + 1
 index = index + 1
-if(single_character == five_character[1]):
+if (single_character == five_character[1]):
     print(single_character + " found at index " + str(index))
     count = count + 1
 index = index + 1
-if(single_character == five_character[2]):
+if (single_character == five_character[2]):
     print(single_character + " found at index " + str(index))
     count = count + 1
 index = index + 1
-if(single_character == five_character[3]):
+if (single_character == five_character[3]):
     print(single_character + " found at index " + str(index))
     count = count + 1
 index = index + 1
-if(single_character == five_character[4]):
+if (single_character == five_character[4]):
     print(single_character + " found at index " + str(index))
     count = count + 1
 index = index + 1
-print(str(count) + " instances of " + single_character + " found in " + five_character)
+if (count == 0):
+    print("No instances of " + single_character + " found in " + five_character)
+elif (count == 1):
+    print(str(count) + " instance of " + single_character + " found in " + five_character)
+else:
+    print(str(count) + " instances of " + single_character + " found in " + five_character)
